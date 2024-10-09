7063d0f0e990299f50eb268499b93ed5792c67ad
Delia62 <iacobutdelia02@gmail.com>
2024-10-09 15:27:31 +0300

    lab1

---
+++ MyFirstARProject/Assets/Moves.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Moves : MonoBehaviour
+{
+    private Animator animator;
+
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        if (animator != null)
+        {
+            
+            if (Input.GetKeyDown(KeyCode.A))
+            {
+               
+                animator.SetBool("isAttacking", true);  
+            }
+
+           
+            if (Input.GetKeyUp(KeyCode.A))
+            {
+               
+                animator.SetBool("isAttacking", false);  
+            }
+        }
+    }
+}
---
+++ MyFirstARProject/Assets/NewFight.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+
+public class NewFight : MonoBehaviour
+{
+    public Animator otherCharacter;   
+    private Animator animator;
+    public float attackDistance = 0.25f; 
+
+    private bool isAttacking = false;
+        
+    void Start()
+    {
+        VuforiaConfiguration.Instance.Vuforia.MaxSimultaneousImageTargets = 4;
+        animator = GetComponent<Animator>();
+
+    }
+
+    void Update()
+    {
+        if (animator != null && otherCharacter != null)
+        {
+            float distanceToOtherCharacter = Vector3.Distance(transform.position, otherCharacter.transform.position);
+
+         
+            if (distanceToOtherCharacter <= attackDistance && !isAttacking)
+            {
+                animator.SetBool("isAttacking", true);  
+                isAttacking = true;                     
+            }
+          
+            else if (distanceToOtherCharacter > attackDistance && isAttacking)
+            {
+                animator.SetBool("isAttacking", false); 
+                isAttacking = false;                    
+            }
+        }
+    }
+}
