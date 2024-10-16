c420a37c55725e8a91a1db4e12fa0794126a73ad
Delia62 <iacobutdelia02@gmail.com>
2024-10-16 15:20:06 +0300

    Tema2

---
+++ Tema2/Assets/ScoreManager.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreManager : MonoBehaviour
+{
+    public Text scoreText;
+    private int score = 0;
+
+    // Modifică IncrementScore pentru a accepta un număr de puncte
+    public void IncrementScore(int points)
+    {
+        score += points;  // Adaugă punctele la scorul curent
+        UpdateScoreText();
+    }
+
+    // Actualizează textul scorului în UI
+    private void UpdateScoreText()
+    {
+        scoreText.text = "Lovituri: " + score.ToString();
+    }
+
+    // Resetează scorul
+    public void ResetScore()
+    {
+        score = 0;
+        UpdateScoreText();
+    }
+}
